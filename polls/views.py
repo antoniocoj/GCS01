f1012b1042b8b6c8952fdb3762b8ea0208a90611
antoniocoj <_antonio_@hotmail.com>
2017-08-27 20:51:45 -0300

    Correcting Style errors

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,9 @@ from django.utils import timezone
 
 from .models import Choice, Question
 
+
 class IndexView(generic.ListView):
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
@@ -38,7 +40,9 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
+
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -54,4 +58,5 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results',
+                                            args=(question.id,)))
